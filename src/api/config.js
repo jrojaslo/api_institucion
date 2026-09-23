8c45aafee67f5d76653c0129e855862e51f4faf9
Jahira <rojas.jahira@gmail.com>
2026-09-22 20:48:29 -0500

    chore: configuracion de endpoints + funciones de servicios base

--- src/api/config.js
+++ src/api/config.js
@@ -1,1 +1,10 @@
-//configuracion de direccion URL piertos, endpoints
+//configuracion de direccion URL, puertos, endpoints
+
+const URL_BASE = "https://api-ofbo.onrender.com/";
+//const URL_BASE_LOCAL = "http://localhost:8080/";
+
+export let end_points = {
+  estudiantes: URL_BASE + "estudiantes/",
+  cursos: URL_BASE + "cursos/",
+  matriculas: URL_BASE + "matriculas/",
+};
--- src/api/services.js
+++ src/api/services.js
@@ -1,1 +1,21 @@
-//peticiones o importaciones
+//servicios/peticiones o importaciones
+
+//fetch ('peticiones http') //funcion nativa del lenguaje que me permite obtener informacion de donde se le requiera
+//      .then
+import { end_points } from "./config";
+
+export function getData() {
+  fetch();
+}
+
+export function createData() {
+  fetch();
+}
+
+export function updateData() {
+  fetch();
+}
+
+export function deleteData() {
+  fetch();
+}
